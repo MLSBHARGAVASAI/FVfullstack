e0953dc3f4c415a0ccaf49515f53c5eba62b277c
MLSBHARGAVASAI <mlsbhargavasai5002@gmail.com>
2024-11-10 19:18:59 +0530

    Fix issues and prepare for deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fruits-vegetables/',
+  base: '/fruits-vegetables/', // Ensure this matches your subdirectory
 })
