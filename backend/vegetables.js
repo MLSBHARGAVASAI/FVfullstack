374ae7e8fcc95a8d55685fe696d7b0101bf03526
MLSBHARGAVASAI <mlsbhargavasai5002@gmail.com>
2024-11-10 17:52:37 +0530

    Add index.js and update cart, fruits, and vegetables files

--- backend/vegetables.js
+++ backend/vegetables.js
@@ -3,9 +3,8 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/fruits-vegetables');
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  await mongoose.connect('mongodb+srv://mlsbhargavasai5002:Mlsbrgv%23665@fvbackend.sx5im.mongodb.net/?retryWrites=true&w=majority&appName=FVbackend');
+  // use await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test'); if your database has auth enabled
 }
 
 
