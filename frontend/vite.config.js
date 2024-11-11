6998e84089829f0d017fbf46cdc75776fe56b6c0
MLSBHARGAVASAI <mlsbhargavasai5002@gmail.com>
2024-11-11 11:06:58 +0530

    Your commit message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fruits-vegetables/',  // Adjust the base path if needed for deployment
+  base: '/frontend/',  // Adjust the base path if needed for deployment
 });
